9ea9dcd0394c3cc6156902fb7da1347050d85816
Anthony Lacruz <anthony.lacruz@og.com.ve>
2018-11-04 14:06:10 -0400

    NavBar terminada junto con sus botones

--- conference-tracker/src/components/layout/NavBar.jsx
+++ conference-tracker/src/components/layout/NavBar.jsx
@@ -2,10 +2,14 @@
 
 const m = require ('mithril')
 
+import NavButton from '../ui/NavButton.jsx'
+
 const NavBar = {
     view : () => 
     <div class="nav-bar">
-
+        <NavButton path={`cpf`} icon={<i class="fas fa-microphone"/>} />
+        <NavButton path={`conferences`} icon={<i class="fas fa-user"/>} />
+        <NavButton path={`entry`} icon={<i class="fas fa-edit"/>} />
     </div>
 }
 
