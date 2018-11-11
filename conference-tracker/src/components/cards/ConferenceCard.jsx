f079f3ada73d3c1e29de1cf8797f4e0e0788631a
Anthony Lacruz <anthony.lacruz@og.com.ve>
2018-11-10 21:02:51 -0400

    Se puede marcar como favorito una reunión, pero no se está viendo el cambio de la estrella

--- conference-tracker/src/components/cards/ConferenceCard.jsx
+++ conference-tracker/src/components/cards/ConferenceCard.jsx
@@ -3,19 +3,50 @@
 const m = require('mithril');
 
 import ConferenceField from './ConferenceField.jsx';
+import CountDownField from './CountDownField.jsx';
+
+import {updateMockData} from '../../store/data.js';
+
+const favCardHandler = (cardObject) =>
+{
+    cardObject["favorite"] = true;
+    
+    updateMockData(cardObject);  
+};
 
 const ConferenceCard = {
-    view : ({ attrs }) =>
+    data : {
+        fav:false
+    }, 
+    view : (vnode) =>
         <div class="conference-card">
             <div class="conference-fields">
-                <ConferenceField fieldValue={`${attrs.conference.name} @ ${attrs.conference.location}`}/>
-                <ConferenceField fieldValue={
-                    <i class="fas fa-star"/>
-                }/>
+                <ConferenceField fieldValue={`${vnode.attrs.conference.name} @ ${vnode.attrs.conference.location}`}/>
+                <a onclick={() => {
+                    vnode.state.fav = true;
+                    favCardHandler(vnode.attrs.conference)
+                }}>
+                {
+                    vnode.state.fav ? 
+                    [
+                        <ConferenceField name="fav" fieldValue={
+                            <i class="fas fa-star"/>
+                        }
+                        />
+                    ]   
+                        : 
+                    [
+                        <ConferenceField name="fav" fieldValue={
+                            <i class="far fa-star"/>
+                        }
+                        />
+                    ]
+                }
+                </a>
             </div>
             <div class="conference-fields">
-                <ConferenceField fieldValue={attrs.conference.date} />
-                <ConferenceField fieldValue={`19 d 20 hr 45m`} />
+                <ConferenceField fieldValue={vnode.attrs.conference.date} />
+                <CountDownField fieldValue={vnode.attrs.conference.date} />
             </div>
         </div>
 };
