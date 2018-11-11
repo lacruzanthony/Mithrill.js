efd4defac9d93be3e158b3f85ecd03a29e555f3f
Anthony Lacruz <anthony.lacruz@og.com.ve>
2018-11-10 21:03:37 -0400

    Se actulizó el campo que muestra el contador

--- conference-tracker/src/components/cards/CFPCard.jsx
+++ conference-tracker/src/components/cards/CFPCard.jsx
@@ -3,6 +3,7 @@
 const m = require('mithril');
 
 import ConferenceField from './ConferenceField.jsx';
+import CountDownField from './CountDownField.jsx';
 
 const CFPCard = {
     view: ({ attrs }) =>
@@ -15,7 +16,7 @@ const CFPCard = {
         </div>
         <div class="conference-fields">
           <ConferenceField fieldValue={attrs.conference.CFPDate} />
-          <ConferenceField fieldValue={`19 d 20 hr 45 m`} />
+          <CountDownField fieldValue={attrs.conference.CFPDate} />
         </div>
       </div>
   };
